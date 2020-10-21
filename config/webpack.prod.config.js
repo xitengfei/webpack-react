1725b06a2ee630dddc64fb19749d0dede69a9fac
Tengfei Xi <xitengfei@github.com>
2020-10-21 10:11:17 +0800

    minify css and js

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -4,11 +4,24 @@ const common = require('./webpack.common.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   output: {
     filename: 'js/[name].[chunkhash:8].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 
+          MiniCssExtractPlugin.loader,
+          'css-loader' 
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,11 +33,23 @@ module.exports = merge(common, {
         collapseWhitespace: true,
       }
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style/[name].[hash].css',
+      chunkFilename: 'style/[id].[hash].css',
+    })
   ],
   optimization: {
     minimizer: [
       new UglifyJsPlugin(),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp:/\.css$/g,
+        cssProcessor:require("cssnano"),
+        cssProcessorPluginOptions:{
+          preset:['default', { discardComments: { removeAll:true } }]
+        },
+        canPrint:true
+      })
     ],
     splitChunks: {
       chunks: 'all',
